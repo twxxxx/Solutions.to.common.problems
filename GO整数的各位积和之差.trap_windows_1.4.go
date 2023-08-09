65939559c40927a831e2249e58206a79a9d97287
twx <133498461+twxxxx@users.noreply.github.com>
2023-08-09 21:11:24 +0800

    Update GO整数的各位积和之差.trap_windows_1.4.go

--- GO整数的各位积和之差.trap_windows_1.4.go
+++ GO整数的各位积和之差.trap_windows_1.4.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+    "fmt"
+)
+
+func main() {
+    var n int
+    fmt.Println("请输入一个正整数：")
+    fmt.Scanf("%d", &n)
+
+    sum := 0
+    product := 1
+    for n > 0 {
+        digit := n % 10
+        sum += digit
+        product *= digit
+        n /= 10
+    }
+
+    fmt.Println("各位积和之差为：", product - sum)
+}
